9918e66c74495cae8f4f331c6d539af97e29c3ce
Jitendra <jitendrasingh28321155@gmail.com>
2023-07-10 16:35:09 +0530

    prolo

--- file_manager/settings.py
+++ file_manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-k2=2(^nm)t4$+se=wlu%tb&byl!d53vd-c1i8u9siyy2y!=-0s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','.now.sh']
 
 
 # Application definition
