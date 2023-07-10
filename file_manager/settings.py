948b537ed5a44c8d5b9b1ce06579a0295b5bd9d9
Jitendra <jitendrasingh28321155@gmail.com>
2023-07-10 17:21:04 +0530

    prolo

--- file_manager/settings.py
+++ file_manager/settings.py
@@ -77,17 +77,25 @@ WSGI_APPLICATION = 'file_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':  'railway',
-        'USER':'postgres',
-        'PASSWORD':str(os.getenv('Password')),
-        'HOST':'containers-us-west-160.railway.app',
-        'PORT':6106,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':  'railway',
+#         'USER':'postgres',
+#         'PASSWORD':str(os.getenv('Password')),
+#         'HOST':'containers-us-west-160.railway.app',
+#         'PORT':6106,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
